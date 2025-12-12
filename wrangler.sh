142ce2eb262d7584b275c5df01e23adf502e9d71
sierra-95 <michaelmachohi@gmail.com>
2025-12-12 13:24:44 +0300

    Db access update: types file env.michaelmachohi

--- wrangler.sh
+++ wrangler.sh
@@ -1,2 +1,7 @@
+npx wrangler d1 create michaelmachohi
+npx wrangler d1 execute michaelmachohi --remote --file=./schema.sql
 npx wrangler d1 list
 npx wrangler d1 execute michaelmachohi --local --command "SELECT * FROM users;"
+curl -X POST https://backend.michaelmachohi.workers.dev/login \
+  -H "Content-Type: application/json" \
+  -d '{"email":"", "password":""}'
