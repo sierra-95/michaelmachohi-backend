d82eef24074d119619230fffc828f9bcc78d7b10
sierra-95 <michaelmachohi@gmail.com>
2025-12-12 13:16:59 +0300

    Db access update: types file env.michaelmachohi

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ import {z} from '@hono/zod-openapi'
 import {AccountSchema} from './openapi/schemas/account';
 
 export interface Env {
-  DB: D1Database;
+  michaelmachohi: D1Database;
 }
 
 export type Variables = {
